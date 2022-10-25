744a1d65dca0cfce3b4a1543ecc6203ef778f8d7
Taylor Allen <taylorallen0913@gmail.com>
2022-10-25 13:58:05 -0700

    done

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+numbers = list(map(int, input().split()))
 
-# ******************************
-# Make your Code
-# ******************************
+sum = 0
+min_num = numbers[0]
+max_num = numbers[0]
+
+for number in numbers:
+    sum += number
+    if number <= min_num:
+        min_num = number
+    else:
+        max_num = number
+
+print(sum - min_num - max_num)
